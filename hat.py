a8f4fd5c5c22355d6d3c922458ea18835e817ba9
Robert Lee <rob3rtyl33@gmail.com>
2017-01-27 18:37:49 -0800

    Fixed some small things

--- hat.py
+++ hat.py
@@ -11,12 +11,12 @@ while display.isNotDone():
         faces.sortArea() #get the biggest one
         face = faces[-1] #last element
         # get the face image
-        xf = face.x -(face.width()/2)
+        xf = face.width()/2
         yf = face.y
         #calculate the hat's position
-        xmust = xf-(hat.width/2)
-        ymust = (hat.height/3)
+        xmust = xf - (hat.width/2)
+        ymust = yf + hat.height
         #blit the stache/mask onto the image
-        img = img.blit(stache, pos=(xmust,ymust), mask = mask)
+        img = img.blit(hat, pos=(xmust,ymust), mask = mask)
 
 img.save(display) #display
--- monacle.py
+++ monacle.py
@@ -20,11 +20,11 @@ while display.isNotDone():
             eye = eyes[-1]
             # these get the upper left corner of the face/nose with respect to original image
             xf = face.x - (face.width()/2)
-            yf = face.y -(face.height()/2)
+            yf = face.y - (face.height()/2)
             xe = eye.x - (eye.width()/2)
             ye = eye.y - (eye.height()/2)
             #calculate the monacle position
-            xmust = xf+xe-(monacle.width/2)+(eye.width()/2)
+            xmust = xf + xe - (monacle.width/2) + (eye.width()/2)
             ymust = yf-ye #+(2*nose.height()/3)
             #blit the stache/mask onto the image
             img = img.blit(monacle, pos=(xmust,ymust), mask = mask)
