05b88f821965dcd2aa9fc12d8ef4bf4ea2fd66d9
Justin <davisandroid242@gmail.com>
2017-01-28 20:01:26 +0000

    working code

--- hat.py
+++ hat.py
@@ -8,19 +8,19 @@ hat = Image("hat.png", sample=True) # load the stache
 mask = hat.createAlphaMask().invert() # load the stache mask
 
 while display.isNotDone():
-    img = cam.getImage()
-    faces = img.findHaarFeatures("face.xml") #find faces
+	img = cam.getImage()
+	faces = img.findHaarFeatures("face.xml") #find faces
 
-    if faces: # if we have a face
-        faces.sortArea() #get the biggest one
-        face = faces[-1] #last element
-        # get the face image
-        xf = face.width()/2
-        yf = face.y
-        #calculate the hat's position
-        xmust = xf - (hat.width/2)
-        ymust = yf + hat.height
-        #blit the stache/mask onto the image
-        img = img.sprite(hat, pos=(xmust,ymust), scale=1.0, rot=0.0, alpha=255)
-
+	if faces: # if we have a face
+		faces.sortArea() #get the biggest one
+		face = faces[-1] #last element
+		# get the face image
+		xf = face.width()/2
+		yf = face.y
+		#calculate the hat's position
+		xmust = xf - (hat.width/2)
+		ymust = yf + hat.height
+		#blit the stache/mask onto the image
+		img = img.blit(hat, pos=(xmust,ymust), mask = mask)
+		img.save(display)
 img.save(display) #display
