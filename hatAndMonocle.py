9f28af197f318fcde8468a9bf290c73fc9873331
Robert Lee <rob3rtyl33@gmail.com>
2017-03-01 19:51:34 -0800

    Fix

--- hatAndMonocle.py
+++ hatAndMonocle.py
@@ -115,12 +115,12 @@ while(cv2.waitKey(30) != 30):
 
             if ex1 < 0:
                 ex1 = 0
-            if ex2 > ew + ex + ex:
-                ex2 = ew + ex + ex
+            if ex2 > w:
+                ex2 = w
             if ey1 < 0:
                 ey1 = 0
-            if ey2 > eh + ey:
-                ey2 = eh + ey
+            if ey2 > h:
+                ey2 = h
 
             monocleHeight = ey2 - ey1
             monocleWidth = ex2 - ex1
